71f7cbb06e440e26c25022f76fdae3f3cbff0547
lferenc <drzony321@onet.pl>
2019-11-20 17:11:21 +0100

    tete

--- src/main/java/pl/krakow/uek/pp5/creditcard/App.java
+++ src/main/java/pl/krakow/uek/pp5/creditcard/App.java
@@ -1,11 +1,31 @@
 package pl.krakow.uek.pp5.creditcard;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import pl.krakow.uek.pp5.creditcard.model.CreditCardFacade;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
         System.out.println("It works :)");
+        SpringApplication.run(App.class, args);
     }
-}
 
-class test{
-System.out.println("test00");
+    @Autowired
+    CreditCardFacade creditCardFacade;
+
+    @EventListener(ApplicationReadyEvent.class)
+    void createSampleCards() {
+        creditCardFacade.register(UUID.randomUUID().toString(), BigDecimal.valueOf(2000));
+        creditCardFacade.register(UUID.randomUUID().toString(), BigDecimal.valueOf(5000));
+        creditCardFacade.register(UUID.randomUUID().toString(), BigDecimal.valueOf(2500));
+    }
+
 }
